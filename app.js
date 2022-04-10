f15457baac5292b3dee29ff86337c6bdf9a94c5e
Do Duc Hong Phuc <phucddhfx13303@funix.edu.vn>
2022-04-10 15:37:26 +0700

    Send response

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log(req)
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>The First Page</title></head>');
+    res.write('<body><h1>Hello from my NodeJS server!</h1></body>');
+    res.write('</html>');
+    res.end();
 });
 
 server.listen(3000);

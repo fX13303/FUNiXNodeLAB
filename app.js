85ab7a9283aaedebbb285496009f078e4e17c8f8
Do Duc Hong Phuc <phucddhfx13303@funix.edu.vn>
2022-04-10 15:51:57 +0700

    chuyen huong request

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
+
+    const url =  req.url;
+    if (url === '/') {
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<head><title>Enter Message</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text"/><button type="submit">Send Us</button></form></body>');
+        res.write('</html>');
+        return res.end();
+    }
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<head><title>The First Page</title></head>');

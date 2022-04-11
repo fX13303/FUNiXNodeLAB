e5cb76bd1ff3cf5a8513b13a4a6d37d1b72cac67
Do Duc Hong Phuc <phucddhfx13303@funix.edu.vn>
2022-04-11 16:58:13 +0700

    xu ly cac route khac nhau

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
-const http = require('http');
-
 const express = require('express')
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('In a middleware');
+app.use('/', (req, res, next) => {
+    console.log('This always run');
     next();
 });
 
-app.use((req, res, next) => {
+app.use('/add-product', (req, res, next) => {
+    res.send('<h1>This is a Add-Product page</h1>');
+});
+
+app.use('/', (req, res, next) => {
     console.log('In another middleware');
     res.send('<h1>Hello from NodeJS</h1>');
 });
 
-const server = http.createServer(app)
-
-server.listen(3000);
+app.listen(3000)
 
 

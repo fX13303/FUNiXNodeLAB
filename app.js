727f02c4697492c7585c30e6b53985659ca3f2a5
Do Duc Hong Phuc <phucddhfx13303@funix.edu.vn>
2022-04-11 08:42:51 +0700

    doc body cua request

--- app.js
+++ app.js
@@ -8,12 +8,22 @@ const server = http.createServer((req, res) => {
         res.setHeader('Content-Type', 'text/html');
         res.write('<html>');
         res.write('<head><title>Enter Message</title></head>');
-        res.write('<body><form action="/message" method="POST"><input type="text"/><button type="submit">Send Us</button></form></body>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"/><button type="submit">Send Us</button></form></body>');
         res.write('</html>');
         return res.end();
     }
     if (url === '/message' && method === 'POST') {
-        fs.writeFileSync('message.txt', 'DUMMY');
+        const body = [];
+        req.on('data', (chunk) => {
+            console.log(chunk);
+            body.push(chunk);
+        });
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            console.log(parsedBody);
+            const message = parsedBody.split('=')[1];
+            fs.writeFileSync('message.txt', message);
+        }); 
         res.statusCode = 302;
         res.setHeader('Location', '/');
         return res.end()

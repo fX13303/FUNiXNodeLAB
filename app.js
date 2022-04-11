466eb28ab78ea444d1febc2c03a9dd1b7d9c6346
Do Duc Hong Phuc <phucddhfx13303@funix.edu.vn>
2022-04-11 23:32:54 +0700

    su dung chuc nang de dieu huong

--- app.js
+++ app.js
@@ -8,13 +8,15 @@ const app = express();
 const adminRouter = require('./routes/admin');
 const shopRouter =  require('./routes/shop');
 
+const rootDir = require('./util/path');
+
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/admin', adminRouter);
 app.use(shopRouter);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, "views", "pagenotfound.html"));
+    res.status(404).sendFile(path.join(rootDir, "views", "pagenotfound.html"));
 });
 
 app.listen(3000)

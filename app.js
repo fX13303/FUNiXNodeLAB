0574d31cc1e69d54093df574bf1cebb2c6bdb6c7
Do Duc Hong Phuc <phucddhfx13303@funix.edu.vn>
2022-04-11 17:26:06 +0700

    phan tich cu phap requests den

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
 const express = require('express')
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use('/', (req, res, next) => {
-    console.log('This always run');
-    next();
-});
+app.use(bodyParser.urlencoded({extended: false}));
+
 
 app.use('/add-product', (req, res, next) => {
-    res.send('<h1>This is a Add-Product page</h1>');
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+});
+
+app.use('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
 });
 
 app.use('/', (req, res, next) => {
-    console.log('In another middleware');
-    res.send('<h1>Hello from NodeJS</h1>');
+    res.send('Welcome to NodeJS!');
 });
 
 app.listen(3000)

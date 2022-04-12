1fa9f8f1f519ed48b435f9e7ca56f85963397ee6
Do Duc Hong Phuc <phucddhfx13303@funix.edu.vn>
2022-04-12 23:51:37 +0700

    lam viec voi ejs

--- app.js
+++ app.js
@@ -6,12 +6,7 @@ const handlebar = require('express-handlebars')
 
 const app = express();
 
-app.engine('hbs', handlebar({
-    layoutsDir: 'views/layouts', 
-    defaultLayout: 'main-layout', 
-    extname: 'hbs'
-}));
-app.set('view engine', 'hbs');
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminData = require('./routes/admin');

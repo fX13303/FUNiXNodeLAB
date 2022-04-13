a7f527903d139a17d5669d07ab87b39c8949d371
Do Duc Hong Phuc <phucddhfx13303@funix.edu.vn>
2022-04-13 09:28:19 +0700

    them controller

--- app.js
+++ app.js
@@ -9,14 +9,13 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-const adminData = require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 const shopRouter =  require('./routes/shop');
 
-const rootDir = require('./util/path');
 
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('/admin', adminData.routes);
+app.use('/admin', adminRoutes);
 app.use(shopRouter);
 
 app.use(express.static(path.join(__dirname, "public")));
--- routes/admin.js
+++ routes/admin.js
@@ -2,26 +2,14 @@ const path = require('path');
 
 const express = require('express');
 
-const rootDir = require('../util/path');
+const productController = require('../controllers/products')
 
 const router = express.Router();
 
 const products = [];
 
-router.get('/add-product', (req, res, next) => {
-    res.render('add-product', {
-        pageTitle: "Add Product", 
-        path: "/admin/add-product",
-        activeAddProduct: true,
-        productCSS: true
+router.get('/add-product', productController.getAddProduct);
 
-    });
-});
+router.post('/add-product', productController.postAddProduct);
 
-router.post('/add-product', (req, res, next) => {
-    products.push({title: req.body.title});
-    res.redirect('/');
-});
-
-exports.routes = router;
-exports.products = products;
+module.exports = router;

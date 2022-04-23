5e9a02f4df27dd09c84df4c37212af08a1c8fbb0
Do Duc Hong Phuc <phucddhfx13303@funix.edu.vn>
2022-04-23 10:04:28 +0700

    xoa cac muc trong gio hanh

--- models/cart.js
+++ models/cart.js
@@ -30,4 +30,20 @@ module.exports = class Cart {
             });
         });
     }
+
+    static deleteProduct(id, productPrice) {
+        fs.readFile(p, (err, fileContent) => {
+            if (err) {
+                return;
+            }
+            const updatedCart = {...JSON.parse(fileContent)};
+            const product = updatedCart.products.find(prod => prod.id === id);
+            const productQty = product.qty;
+            updatedCart = updatedCart.products.filter(prod => prod.id !== id);
+            updatedCart.totalPrice = cart.totalPrice - productQty * productPrice;
+            fs.writeFile(p, JSON.stringify(cart), err => {
+                console.log(err);
+            });
+        })
+    }
 }

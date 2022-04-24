a636710fcc7a62fcc00426155925ffbf68a8395b
Do Duc Hong Phuc <phucddhfx13303@funix.edu.vn>
2022-04-24 09:38:10 +0700

    chen du lieu va tao san pham

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,13 +13,17 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const description = req.body.description;
   const price = req.body.price;
-  const product = new Product(null, title, imageUrl, description, price);
-  product.save()
-  .then(() => {
-    res.redirect('/');
+  Product.create({
+    title: title,
+    price: price,
+    imageUrl: imageUrl,
+    description: description
+  }).then((result) => {
+    // console.log(result)
+    console.log('Create product');
   })
   .catch(err => console.log(err));
-};
+}
 
 exports.getEditProduct = (req, res, next) => {
   const editMode = req.query.edit;
@@ -65,4 +69,4 @@ exports.getProducts = (req, res, next) => {
       path: '/admin/products'
     });
   });
-};
+}

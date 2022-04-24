1f5fa9ee8317158a5339d850a0b403f0f1a94e99
Do Duc Hong Phuc <phucddhfx13303@funix.edu.vn>
2022-04-24 10:17:38 +0700

    cap nhat san pham

--- controllers/admin.js
+++ controllers/admin.js
@@ -31,17 +31,16 @@ exports.getEditProduct = (req, res, next) => {
     res.redirect('/');
   }
   const prodId = req.params.productId;
-  Product.findById(prodId, product => {
-    if (!product) {
-      return res.redirect('/');
-    }
+  Product.findByPk(prodId)
+  .then(product => {
     res.render('admin/edit-product', {
       pageTitle: 'Edit Product',
       path: '/admin/edit-product',
       editing: editMode,
       product: product
     });
-  });
+  })
+  .catch(err => console.log(err))
 };
 
 exports.postEditProduct = (req, res, next) => {
@@ -50,9 +49,19 @@ exports.postEditProduct = (req, res, next) => {
   const updatedPrice = req.body.price;
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
-  const updatedProduct = new Product  (prodId, updatedTitle, updatedImageUrl, updatedDesc, updatedPrice);
-  updatedProduct.save();
-  res.redirect('/admin/products');
+  Product.findByPk(prodId)
+  .then(product => {
+    product.title = updatedTitle;
+    product.price = updatedPrice;
+    product.imageUrl = updatedImageUrl;
+    product.description = updatedDesc;
+    return product.save();
+  })
+  .then(result => {
+    console.log("Updated Successfully");
+    res.redirect('/admin/products');
+  })
+  .catch(err => console.log(err))
 }
 
 exports.postDeleteProduct = (req, res, next) => {

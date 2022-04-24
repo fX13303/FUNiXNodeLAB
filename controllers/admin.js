f839339ac8040451c5a0b2f4387350b4a15592c1
Do Duc Hong Phuc <phucddhfx13303@funix.edu.vn>
2022-04-24 10:24:37 +0700

    xoa san pham

--- controllers/admin.js
+++ controllers/admin.js
@@ -21,6 +21,7 @@ exports.postAddProduct = (req, res, next) => {
   }).then((result) => {
     // console.log(result)
     console.log('Create product');
+    res.redirect('/admin/products');
   })
   .catch(err => console.log(err));
 }
@@ -66,8 +67,15 @@ exports.postEditProduct = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.deleteById(prodId);
-  res.redirect('/admin/products');
+  Product.findByPk(prodId)
+  .then(product => {
+    return product.destroy();
+  })
+  .then(result => {
+    console.log("Delete Successfully");
+    res.redirect('/admin/products');
+  })
+  .catch(err => console.log(err));
 }
 
 exports.getProducts = (req, res, next) => {

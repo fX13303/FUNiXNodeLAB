dc0bade6ffdce7cfd932b93d5b957ff9805feddb
Do Duc Hong Phuc <phucddhfx13303@funix.edu.vn>
2022-04-25 10:38:08 +0700

    tao san pham"

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,16 +13,11 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const description = req.body.description;
   const price = req.body.price;
-  req.user
-  .createProduct({
-    title: title,
-    price: price,
-    imageUrl: imageUrl,
-    description: description
-  })
+  const product = new Product(title, price, description, imageUrl);
+  product.save()
   .then((result) => {
     // console.log(result)
-    console.log('Create product');
+    console.log('Created product');
     res.redirect('/admin/products');
   })
   .catch(err => {

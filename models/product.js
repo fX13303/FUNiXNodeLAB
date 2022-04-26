b2dae2bea361516a13ecde915d178013ca4911ae
Do Duc Hong Phuc <phucddhfx13303@funix.edu.vn>
2022-04-26 16:36:27 +0700

    tim nap tat ca cac san pham

--- models/product.js
+++ models/product.js
@@ -11,14 +11,26 @@ class Product {
   save(){
     const db = getDb();
     return db.collection('products')
-      .insertOne(this)
-      .then(result => {
-        console.log(result)
-      })
-      .catch(err => {
-        console.log(err)
-      });
+    .insertOne(this)
+    .then(result => {
+      console.log(result)
+    })
+    .catch(err => {
+      console.log(err)
+    });
   } 
+  static fetchAll() {
+    const db = getDb();
+    return db.collection('products')
+    .find()
+    .toArray()
+    .then(products => {
+      return products;
+    })
+    .catch(err => {
+      console.log(err);
+    })
+  }
 }
 
 module.exports = Product;

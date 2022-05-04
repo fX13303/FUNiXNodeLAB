dbf47fdc3c82914faf84b8aadda5c7e9cd987c01
Do Duc Hong Phuc <phucddhfx13303@funix.edu.vn>
2022-05-04 11:33:27 +0700

    sua loi

--- controllers/shop.js
+++ controllers/shop.js
@@ -59,6 +59,7 @@ exports.postCart = (req, res, next) => {
     return req.user.addToCart(product);
   }).then(result => {
     console.log(result);
+    res.redirect('/cart');
   })
 }
 //   let fetchedCart;

31e0981b0f1d62e8bc8341a9e007d19915eb99a3
Do Duc Hong Phuc <phucddhfx13303@funix.edu.vn>
2022-05-07 11:11:08 +0700

    su dung quan he trong Mongoose

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
     User.findById('6275ecff2f1206cf4eefd5e9')
     .then(user => {
-        req.user = new User (user.name, user.email, user.cart, user._id);
+        req.user = user;
         next();
     })
     .catch(err => console.log(err))

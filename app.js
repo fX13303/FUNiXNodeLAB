cb74cd81f59717def99dfeca021e321344d2cfc2
Do Duc Hong Phuc <phucddhfx13303@funix.edu.vn>
2022-05-07 10:53:24 +0700

    them va su dung model nguoi dung

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose');
 
 
 const errorController = require('./controllers/error'); 
-// const User = require('./models/user');
+const User = require('./models/user');
 
 const app = express();
 
@@ -19,14 +19,14 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use((req, res, next) => {
-//     User.findByPk('6271d74e5644ecc943c14fce')
-//     .then(user => {
-//         req.user = new User (user.name, user.email, user.cart, user._id);
-//         next();
-//     })
-//     .catch(err => console.log(err))
-// })
+app.use((req, res, next) => {
+    User.findById('6275ecff2f1206cf4eefd5e9')
+    .then(user => {
+        req.user = new User (user.name, user.email, user.cart, user._id);
+        next();
+    })
+    .catch(err => console.log(err))
+})
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -35,6 +35,18 @@ app.use(errorController.get404);
 
 mongoose.connect('mongodb+srv://root:hongPHUC.2002@cluster0.cn8ve.mongodb.net/shop')
     .then(result => {
+        User.findOne().then(user => {
+            if (!user) {
+                const user = new User({
+                    name: 'max',
+                    email: 'test@test.com',
+                    cart: {
+                        items: []
+                    }
+                })
+                user.save();
+            }
+        })
         app.listen(3000);
     })
     .catch(err => {

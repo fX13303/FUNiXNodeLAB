bf04991aff3351afc53601f4cdb6562459c04e19
Do Duc Hong Phuc <phucddhfx13303@funix.edu.vn>
2022-05-07 13:17:02 +0700

    nhan va hien thi cac don hang

--- controllers/shop.js
+++ controllers/shop.js
@@ -77,8 +77,7 @@ exports.postCartDeleteItems = (req, res, next) => {
 }
 
 exports.getOrders = (req, res, next) => {
-  req.user
-  .getOrders({include: ['products']})
+  Order.find({'user.userId': req.user._id})
   .then(orders => {
     res.render('shop/orders', {
       path: '/orders',
